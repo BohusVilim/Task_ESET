6f3387565bd7354aa46b4d3d3166a5c9b02d985a
Bohuš Vilim <121969662+BohusVilim@users.noreply.github.com>
2023-06-02 10:24:35 +0200

    update

--- Task_ESET/Parser.cs
+++ Task_ESET/Parser.cs
@@ -117,6 +117,7 @@ namespace Task_ESET
                                     }
 
                                     list.Add(infectedFile);
+
                                 }
                             }
                         }
